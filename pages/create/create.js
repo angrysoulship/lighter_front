da74b57c1f785b7ca3fa043be461d91e10761f9f
Ellie Wang <yuetingw@outlook.com>
2021-07-30 20:56:47 +0800

    create page add back btn

--- pages/create/create.js
+++ pages/create/create.js
@@ -95,6 +95,17 @@ Page({
     this.setData({step: 1})
   },
 
+  goBack: function(e) {
+    this.setData({step: 0})
+  },
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  
   onLoad: function (options) {
 
   },
