e26e2f6cd60eecf60da1a05408381692d601b207
eo hao <hash1hao@gmail.com>
2021-08-02 15:52:17 +0800

    User

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,7 +5,81 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabCur: 0,
+    step: 0,
+    emojis: {
+      active: 0,
+      items: [{
+        name:'delighted',
+        cnName: '开心',
+        id: 0,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/delighted.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/delightedEmoji.png'
+      },
+      {
+        name:'excited',
+        cnName: '上头！',
+        id: 1,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/excited.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/excitedEmoji.png'
+      },
+      {
+        name:'angry',
+        cnName: '好气！',
+        id: 2,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/angry.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/angryEmoji.png'
+      },
+      {
+        name:'annoyed',
+        cnName: '好烦！',
+        id: 3,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/annoyed.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/annoyedEmoji.png'
+      },
+      {
+        name:'shameful',
+        cnName: '好失败',
+        id: 4,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/shameful.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/shamefulEmoji.png'
+      },
+      {
+        name:'worried',
+        cnName: '担心',
+        id: 5,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/worried.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/worriedEmoji.png'
+      },
+      {
+        name:'sad',
+        cnName: '难过',
+        id: 6,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/sad.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/sadEmoji.png'
+      },
+      {
+        name:'tired',
+        cnName: '累了',
+        id: 7,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/tired.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/tiredEmoji.png'
+      },
+      {
+        name:'relaxed',
+        cnName: '放松',
+        id: 8,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/relaxed.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/relaxedEmoji.png'
+      },
+      {
+        name:'serene',
+        cnName: '平静',
+        id: 9,
+        url: 'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emoji_v1/serene.png',
+        emojiUrl:'https://projectlighter.oss-cn-shenzhen.aliyuncs.com/emojiOnly/sereneEmoji.png'
+      }]
+    }
   },
 
   /**
