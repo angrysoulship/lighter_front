ef3b51f07fa137810037dfb20e1a01b314324c98
marshall chen <marshallchan666@outlook.com>
2021-08-04 12:54:32 +0800

    layout done,need realtime data

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,8 +1,119 @@
+import * as echarts from '../../components/ec-canvas/echarts';
 
 const app = getApp()
+function initPieChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  var option = {
+    // backgroundColor: "#ffffff",
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: ['20%', '40%'],
+      data: [{
+        value: 55,
+        name: '北京'
+      }, {
+        value: 20,
+        name: '武汉'
+      }, {
+        value: 10,
+        name: '杭州'
+      }, {
+        value: 20,
+        name: '广州'
+      }, {
+        value: 38,
+        name: '上海'
+      }]
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+
+function initLineChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    title: {
+      text: '测试下面legend的红色区域不应被裁剪',
+      left: 'center'
+    },
+    legend: {
+      data: ['A', 'B', 'C'],
+      top: 50,
+      left: 'center',
+      // backgroundColor: 'red',
+      z: 100
+    },
+    grid: {
+      containLabel: true
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      // show: false
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+      // show: false
+    },
+    series: [{
+      name: 'A',
+      type: 'line',
+      smooth: true,
+      data: [18, 36, 65, 30, 78, 40, 33]
+    }, {
+      name: 'B',
+      type: 'line',
+      smooth: true,
+      data: [12, 50, 51, 35, 70, 30, 20]
+    }, {
+      name: 'C',
+      type: 'line',
+      smooth: true,
+      data: [10, 30, 31, 50, 40, 20, 10]
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
 
 Page({
   data: {
+    ecPie: {
+      onInit: initPieChart
+    },
+    ecLine: {
+      onInit: initLineChart
+    },
     tabCur: 0,
     step: 0,
     emojis: {
@@ -79,11 +190,12 @@ Page({
       }]
     }
   },
-
+  
 
   onLoad: function () {
     this.setData({
       user: app.globalData.user
     })
   }
+  
 })
